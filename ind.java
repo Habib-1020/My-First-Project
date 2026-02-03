4c32b6448d9c4fa70dee5fadcc747d71fad9a7b7
Habib Shahriar Ridoy <habibridoy622@gmail.com>
2026-02-03 14:43:48 +0600

    Update ind.java

--- ind.java
+++ ind.java
@@ -2,5 +2,7 @@ public class ind{
     public static void main(String[] args) {
         System.out.println("Hello GitHub, I am coming");
         System.out.println("Coming soon");
+        System.out.println("Already coming");
+        
     }
 }
